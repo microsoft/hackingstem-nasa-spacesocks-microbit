f43062599e2c2a7c1c7df24034e269bd61fc4b5f
Jeremy Franklin-Ross <v-jerfra@microsoft.com>
2019-06-10 09:21:09 -0700

    updated comments license reademe

---
+++ README.md
@@ -0,0 +1,10 @@
+# Introduction
+Micropython code for HackingSTEM Astro Socks lesson plan adapted for micro:bit
+
+# Getting Started
+1. Download lesson assets at http://aka.ms/hackingSTEM
+1. Assemble your electronics
+1. Use [Mu](https://codewith.mu/) to flash glove.py to your glove [micro:bit](https://microbit.org/) microcontroller
+1. Use [Mu](https://codewith.mu/) to flash robotic_hand.py to your hand [micro:bit](https://microbit.org/) microcontroller
+1. Verify data interactions in Excel from (hand microcontroller)
+1. Ready, Set, Science!
--- astro_socks.py
+++ astro_socks.py
@@ -1,26 +1,28 @@
 # ------------__ Hacking STEM astro_socks.py micro:bit __-----------
-#  For use with the TODO:[Add lesson title] 
-#  lesson plan available from Microsoft Education Workshop at 
-#  http://aka.ms/hackingSTEM
+#  For use with the Astro Socks lesson plan available from 
+#  Microsoft Education Workshop at http://aka.ms/hackingSTEM
 #
 #  Overview: This project reads voltage on 4 GPIO pins and writes those values
-#  to serial, and reads serial and looks for keywords.
+#  to serial and radio.
 # 
 #  Pins:
-#  Pin 0 = Toe Sensor
-#  Pin 1 = First Mid-foot Sensor
-#  Pin 2 = Second Mid-foot Sensor
+#  Pin 0 = Phalanges Sensor
+#  Pin 1 = Metatarsals Sensor
+#  Pin 2 = Tarsals Sensor
 #  Pin 3 = Ankle Sensor
-#  Pin 16 = radio lockout
 #  
+#  To set radio channel, hold both buttons for 1 second and then use 
+#  buttons to increment/decrement channel. See astro_socks_receiver.py 
+#  for the companion receiver program.
+#
 #  This project uses a BBC micro:bit microcontroller, information at:
 #  https://microbit.org/
 #
 #  Comments, contributions, suggestions, bug reports, and feature
 #  requests are welcome! For source code and bug reports see:
-#  http://github.com/[TODO github path to Hacking STEM]
+#  http://github.com/microsoft/[TODO github path to Hacking STEM]
 #
-#  Copyright 2018, Adi Azulay
+#  Copyright 2019, Jeremy Franklin-Ross & Adi Azulay
 #  Microsoft EDU Workshop - HackingSTEM
 #  MIT License terms detailed in LICENSE.txt
 # ===---------------------------------------------------------------===
@@ -43,7 +45,6 @@ TOE_SENSOR = pin0
 FIRST_MID_SENSOR = pin1
 SECOND_MID_SENSOR = pin2
 ANKLE_SENSOR = pin3
-RADIO_LOCKOUT = pin16 
 
 def process_sensors():
     # Reads voltage of from each pin attached to a pressure sensor
@@ -52,36 +53,6 @@ def process_sensors():
     first_mid_reading = FIRST_MID_SENSOR.read_analog()
     second_mid_reading = SECOND_MID_SENSOR.read_analog()
     ankle_reading = ANKLE_SENSOR.read_analog()
-
-
-#=============================================================================#
-#---------------The Code Below Here is for Excel Communication----------------#
-#=============================================================================#
-
-# Array to hold the serial data
-parsedData = [0] * 5
-
-
-def getData():
-    #   This function gets data from serial and builds it into a string
-    global parsedData, builtString
-    builtString = ""
-    while uart.any() is True:
-        byteIn = uart.read(1)
-        if byteIn == b'\n':
-            continue
-        byteIn = str(byteIn)
-        splitByte = byteIn.split("'")
-        builtString += splitByte[1]
-    parseData(builtString)
-    return (parsedData)
-
-
-def parseData(s):
-    #   This function seperates the string into an array
-    global parsedData
-    if s != "":
-        parsedData = s.split(",")
 
 #=============================================================================#
 #------------------------------Main Program Loop------------------------------#
@@ -112,16 +83,14 @@ while (True):
             
 
     process_sensors()
-    serial_in_data = getData()
 
     # Create a string of the data to be sent
     data_to_send = ",{},{},{},{}".format(toe_reading, first_mid_reading, second_mid_reading, ankle_reading)
 
-    if (serial_in_data[0] != "#pause"):
-        # Send data to radio
-        radio.send(data_to_send)
+    # Send data to radio
+    radio.send(data_to_send)
 
-        # uart is the micro:bit command for serial
-        uart.write(data_to_send + EOL)
+    # uart is the micro:bit command for serial
+    uart.write(data_to_send + EOL)
 
     sleep(DATA_RATE)
